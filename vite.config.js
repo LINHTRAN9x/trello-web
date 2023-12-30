329ef6a6e6d659dc43c04a227f9940266dc5752e
LINHTRAN9X <tranvanlinh99x@gmail.com>
2023-12-30 10:55:18 +0700

    appbar p1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr' //Vite plugin to transform SVGs into React components.
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr()
+  ],
   // base: './'
   resolve: {
     alias: [
       { find: '~', replacement: '/src' } //thay the ralative path thanh xu ly ~/ (absolute) cho no gon gang.
     ]
-  }
+  },
 })
