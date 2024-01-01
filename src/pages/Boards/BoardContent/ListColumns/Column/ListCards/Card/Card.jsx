175198da63e09b428bffb6fbcb14460e402f9e0b
LINHTRAN9X <tranvanlinh99x@gmail.com>
2024-01-01 15:05:25 +0700

    mock_sort _data & chia Component

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -8,19 +8,9 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 
-function Card({ temporaryHideMedia }) {
-  if (temporaryHideMedia === true) {
-    return (
-      <MuiCard sx={{
-        cursor: 'pointer',
-        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
-      }}>
-        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
-          <Typography>Card test 01</Typography>
-        </CardContent>
-      </MuiCard>
-    )
+function Card({ card }) {
+  const shouldShowCardActions = () => {
+    return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
   }
   return (
     <MuiCard sx={{
@@ -28,19 +18,18 @@ function Card({ temporaryHideMedia }) {
       boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
       overflow: 'unset'
     }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="https://t3.ftcdn.net/jpg/05/71/06/76/360_F_571067620_JS5T5TkDtu3gf8Wqm78KoJRF1vobPvo6.jpg"
-        title="green iguana"
-      />
+      {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover}/>
+      }
       <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
-        <Typography>LinhTran hayen</Typography>
+        <Typography>{card.title}</Typography>
       </CardContent>
+      {shouldShowCardActions() &&
       <CardActions sx={{ p: '0 4px 8px 4px' }}>
-        <Button size="small" startIcon={<GroupIcon />}>22</Button>
-        <Button size="small" startIcon={<CommentIcon />}>10</Button>
-        <Button size="small" startIcon={<AttachmentIcon />}>12</Button>
+        {!!card?.memberIds?.length && <Button size="small" startIcon={<GroupIcon />}>{card.memberIds?.length}</Button>}
+        {!!card?.comments?.length && <Button size="small" startIcon={<CommentIcon />}>{card.comments?.length}</Button>}
+        {!!card?.attachments?.length && <Button size="small" startIcon={<AttachmentIcon />}>{card.attachments?.length}</Button>}
       </CardActions>
+      }
     </MuiCard>
   )
 }
